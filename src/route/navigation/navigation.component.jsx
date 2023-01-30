67ef97fdbbb16c3fe0efad98513129b68af74f11
Amminadab <amminadabeliasfant@gmail.com>
2023-01-31 00:05:06 +0300

    added a new directory to store routes like is Navigation

---
+++ src/route/navigation/navigation.component.jsx
@@ -0,0 +1,50 @@
+import logo from "../../asset/globe-logo.png";
+import { Link, Outlet } from "react-router-dom";
+
+import "./navigation.style.css";
+
+const Navigation = () => {
+  return (
+    <>
+      <header>
+        <div className="nav">
+          <div className="logo-block">
+            <img className="nav-logo" src={logo} alt="globe logo" />
+            <p className="nav-text">Countries</p>
+          </div>
+          <div className="nav-linkes">
+            <ul className="nav-links-container">
+              <li>
+                <Link className="nav-link" to="/">
+                  Home
+                </Link>
+              </li>
+              <li>
+                <Link className="nav-link" to="/search">
+                  Search
+                </Link>
+              </li>
+              <li>
+                <Link className="nav-link" to="/uncolonized">
+                  Uncolonized
+                </Link>
+              </li>
+              <li>
+                <Link className="nav-link" to="/neighbour">
+                  Neighbours
+                </Link>
+              </li>
+              <li>
+                <Link className="nav-link" to="/continent">
+                  Continent
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </header>
+      <Outlet />
+    </>
+  );
+};
+export default Navigation;
