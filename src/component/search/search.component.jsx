10ca7a8460c86fc367a458043b196d2c0b64efc2
Amminadab <amminadabeliasfant@gmail.com>
2023-02-23 14:49:55 +0300

    finalized the design

--- src/component/search/search.component.jsx
+++ src/component/search/search.component.jsx
@@ -7,32 +7,110 @@ import "./search.query.css";
 const Search = () => {
   const { countries } = useContext(CountriesContext);
   // console.log(countries);
+  const countryReference = countries;
   const [filteredCountries, setFilteredCountries] = useState(countries);
   const [searchedText, setSearchedText] = useState("");
 
+  const [specificCountry, setSpecificCountry] = useState(countries);
+
+  const [selectedContient, setSelectedContient] = useState("All Continent");
+
   useEffect(() => {
-    const newFilteredList = countries.filter((country) => {
+    setSpecificCountry(countries);
+  }, [countries]);
+
+  let allContientsName = new Set([]);
+
+  countries.map((country) => {
+    const continentName = country.continents[0];
+    allContientsName.add(continentName);
+    return 0;
+  });
+
+  const allContientsNameArray = [...allContientsName];
+
+  useEffect(() => {
+    // console.log(specificCountry);
+    // console.log(countries);
+
+    const newFilteredList = specificCountry.filter((country) => {
       const name = country.name.common.toLocaleLowerCase();
       return name.includes(searchedText.toLocaleLowerCase());
     });
     setFilteredCountries(newFilteredList);
-  }, [countries, searchedText]);
+  }, [searchedText, specificCountry, countries]);
+
+  useEffect(() => {
+    document.getElementById("All").classList.add("active");
+  }, []);
+
+  const continentFilter = (e) => {
+    e.preventDefault();
+
+    const buttonToChange = document.getElementById(`${e.target.name}`);
+    allContientsNameArray.map((contient) =>
+      document.getElementById(`${contient}`).classList.remove("active")
+    );
+    document.getElementById("All").classList.remove("active");
+    buttonToChange.classList.add("active");
+    if (e.target.name === "All") {
+      setFilteredCountries(countryReference);
+      setSpecificCountry(countryReference);
+      setSelectedContient("All Continent");
+    } else {
+      const byContientFilteredCountry = countryReference.filter((country) => {
+        // console.log(country.continents[0].toLocaleLowerCase());
+        // console.log(e.target.name.toLocaleLowerCase());
+        return (
+          e.target.name.toLocaleLowerCase() ===
+          country.continents[0].toLocaleLowerCase()
+        );
+      });
+      setSpecificCountry([...byContientFilteredCountry]);
+
+      setFilteredCountries(byContientFilteredCountry);
+      setSelectedContient(e.target.name);
+    }
+  };
 
   return (
-    <section className="section-search">
-      <div className="search-block">
-        <input
-          className="search-box"
-          placeholder="Search any country"
-          type="search"
-          value={searchedText}
-          onChange={(event) => {
-            setSearchedText(event.target.value);
-          }}
-        />
+    <>
+      <div className="aligner">
+        <p> Filter Continent</p>
+        <ul className="filter-list">
+          {allContientsNameArray[0] &&
+            allContientsNameArray.map((contient) => (
+              <button
+                key={contient}
+                name={contient}
+                id={contient}
+                className="country-filter-btn"
+                onClick={continentFilter}
+              >
+                {contient}
+              </button>
+            ))}
+          <button name="All" id="All" onClick={continentFilter}>
+            All
+          </button>
+        </ul>
       </div>
-      <CountriesContainer countries={filteredCountries} />
-    </section>
+
+      <section className="section-search">
+        <div className="search-block">
+          <input
+            className="search-box"
+            placeholder={`Search From ${selectedContient}`}
+            type="search"
+            value={searchedText}
+            onChange={(event) => {
+              setSearchedText(event.target.value);
+            }}
+          />
+        </div>
+        <CountriesContainer countries={filteredCountries} />
+      </section>
+    </>
   );
 };
 export default Search;
